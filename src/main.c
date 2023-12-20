c8e1f98666e606d0bc322df19a9b8575b5e388ed
imma-Spring <jairjair511@gmail.com>
2023-12-20 14:38:11 -0500

    implemented [adding imports to an array only once]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include "import.h"
+#include <stdio.h>
 #include <stdlib.h>
 int main(void) {
-  free(combine_files("test.salmon"));
+  char *src_code = combine_files("test.salmon");
+  printf("%s\n", src_code);
+  free(src_code);
   return 0;
 }
